d147543a48ff7af3db207f07b09de9b08a505bcd
alicemorey <alicemmorey@gmail.com>
2025-04-20 09:52:52 +0200

    more styles and layout for archive

---
+++ scripts.js
@@ -0,0 +1,15 @@
+<script>
+function openModal(imageSrc, captionText) {
+  const modal = document.getElementById("imageModal");
+  const modalImg = document.getElementById("modalImage");
+  const caption = document.getElementById("modalCaption");
+
+  modal.style.display = "block";
+  modalImg.src = imageSrc;
+  caption.textContent = captionText;
+}
+
+function closeModal() {
+  document.getElementById("imageModal").style.display = "none";
+}
+</script>
